caa54958f209fcc6c0fde233059a907b3bc66bed
ngvanchung <nguyenvanchung85@gmail.com>
2020-08-16 01:07:03 +0700

    Create releasenotes.groovy

---
+++ vars/releasenotes.groovy
@@ -0,0 +1,16 @@
+import java.io.*
+import groovy.io.*
+
+def call(Map config=[:]) {
+    def dir = 
+    new File(dir.path + "/releasenotes.txt").withWriter('utf-8') {
+        writer ->
+            dir.eachFileRecurse(FileType.ANY) {file ->
+                if (file.isDirectory()) {
+                    writer.writeLine(file.name)
+                } else {
+                    writer.writeLine(file.name + '\t' + file.length())
+                }
+            }
+    }
+}
